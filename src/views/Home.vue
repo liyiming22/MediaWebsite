cf49ed236b8aef4d0f5f10e032e95c567da0d591
Yeoman_Li <Yeoman_Li@outlook.com>
2018-12-04 17:21:57 +0800

    start router refactoring

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <div class="title">
+      <h1><i class="fa fa-braille"></i> 媒体存档</h1>
+    </div>   -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
+  name: 'home'
 }
 </script>
