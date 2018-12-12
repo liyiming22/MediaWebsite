2757005a4598afe48c71a22c6d99385eaa35c9ef
Yeoman_Li <Yeoman_Li@outlook.com>
2018-12-12 14:39:28 +0800

    details page finished

--- src/components/MediaDetails.vue
+++ src/components/MediaDetails.vue
@@ -13,6 +13,8 @@
               {{media.description}}
             </p>
           </div>
+          <button class="toClipBoardBtn button" :data-clipboard-text="media.mediaURL">复制链接到剪贴板</button>
+          <button class="openPlayerBtn button">使用MyVLC打开</button>
         </div>
       </div>
     </div>
@@ -20,20 +22,38 @@
 </template>
 
 <script>
-  export default {
-    props: ['media'],
+import ClipboardJS from 'clipboard'
+export default {
+  props: ['media'],
+  mounted () {
+    const clipboard = new ClipboardJS('.toClipBoardBtn')
   }
+}
 </script>
 
-<style>
+<style scoped>
   .media-details {
     border-bottom: 2px solid #F5F5F5;
     padding: 30px 0;
   }
   .media-details__description {
     padding-top: 30px;
+    margin-bottom: 20px;
   }
   .media-details__description h3 {
     margin: 5px 0 30px 0;
+  }
+  .toClipBoardBtn,
+  .openPlayerBtn {
+    border-radius: 0;
+    width: 50%;
+  }
+
+  .openPlayerBtn {
+    background: #67C23A;
+  }
+
+  .openPlayerBtn:hover {
+    background: #9dc237;
   }
 </style>
